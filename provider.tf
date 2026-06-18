fc0efc2ea94cf5bff0ad4ef9e2319d85d494cf7b
manoharmotam <b3stsinceday1@gmail.com>
2026-06-18 07:45:52 +0000

    Updated: providers

--- provider.tf
+++ provider.tf
@@ -1,8 +1,23 @@
 terraform {
   required_providers {
     http = {
-	source = "hashicorp/http"
-	version = "5.6.0"
+      source  = "hashicorp/http"
+      version = "5.6.0"
+    }
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.48.0"
     }
   }
+  backend "s3" {
+    bucket = "manoharmotam-terraform-state"
+    key = "functions/func-demo.tfstate"
+    use_lockfile = true
+    region = "us-east-1"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
 }

13970176eee6dae3f43047757a963d107af12563
manoharmotam <b3stsinceday1@gmail.com>
2026-06-19 14:04:58 +0000

    Add: split function example

--- functions/output.tf
+++ functions/output.tf
@@ -9,3 +9,9 @@ output "instance_data" {
 output "rev_ip" {
     value = strrev(data.aws_instance.get_data.private_ip) #prints the string in reverse(172.31.66.44 ==> 44.66.31.172 )
 }
+
+#Separates the string with provided delimiter and returns a list (172.31.66.44 ==> ["172", "31", "66", "44"])
+output "separator" {
+    value = split(".",data.aws_instance.get_data.private_ip)
+}
+

4cb6746327b4c0965f8f3e9b5abf01f10ffcdbd9
manoharmotam <b3stsinceday1@gmail.com>
2026-06-23 17:30:22 +0000

    Add: modules simple example

--- modules/provider.tf
+++ modules/provider.tf
@@ -5,4 +5,11 @@ terraform {
         version = "6.48.0"
     }
   }
+  backend "s3" {
+    bucket = "value"
+    key = "value"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
 }

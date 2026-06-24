3869964d347dcaeaa19683571be6e368a935bdec
manoharmotam <b3stsinceday1@gmail.com>
2026-06-24 09:11:59 +0000

    Add: modules simple ec2 example

--- provider.tf
+++ provider.tf
@@ -1,8 +1,15 @@
-# terraform {
-#   required_providers {
-#     aws ={
-#         source = "hashicorp/aws"
-#         version = "6.48.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "6.48.0"
+    }
+  }
+  backend "s3" {
+    bucket = "manoharmotam-remote-state-test"
+    key = "testing.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
